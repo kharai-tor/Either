71407da463fb6e787585eebe969e9122c5578ce5
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2025-03-10 22:40:43 +0400

    Concise kinds of types tests

--- Either/Either.Test/KindsOfTypesTests.cs
+++ Either/Either.Test/KindsOfTypesTests.cs
@@ -4,54 +4,22 @@ namespace RhymesOfUncertainty.Test;
 
 public class KindsOfTypesTests
 {
-    [Fact]
-    public async Task Given_Switch_Statement_With_Matching_Generic_Types_Succeed()
+    [Theory]
+    [InlineData(SwitchType.Stmt, new[] { "List<int>", "Dictionary<bool, double>", "Either<float, HashSet<decimal>>" }, new[] { "List<int>", "Dictionary<bool, double>|Either<float, HashSet<decimal>>", "null" })]
+    [InlineData(SwitchType.Expr, new[] { "List<int>", "Dictionary<bool, double>", "Either<float, HashSet<decimal>>" }, new[] { "List<int>", "Dictionary<bool, double>", "Either<float, HashSet<decimal>>", "null" })]
+    public async Task Switch_With_Matching_Generic_Types_Succeeds(SwitchType switchType, string[] typesToCheck, string[] casesChecked)
     {
-        var code = "using System.Collections.Generic;"
-            + Shared.Structs
-            + @"
-class C
-{
-    void M(Either<List<int>, Dictionary<bool, double>, Either<float, HashSet<decimal>>> x)
-    {
-        switch (x.Value)
-        {
-            case List<int>:
-                break;
-            case Dictionary<bool, double>:
-            case Either<float, HashSet<decimal>>:
-                break;
-            case null:
-                break;
-        }
-    }
-}
-";
+        var code = Shared.GenerateSwitch(switchType, typesToCheck, casesChecked, new() { Usings = ["System.Collections.Generic"] });
         await VerifyCS.VerifyAnalyzerAsync(code);
     }
 
-    [Fact]
-    public async Task Given_Switch_Statement_With_Mismatching_Generic_Type_Fail()
+    [Theory]
+    [InlineData(SwitchType.Stmt, new[] { "int", "List<int>" }, new[] { "int", "t:List<bool>", "null" })]
+    [InlineData(SwitchType.Expr, new[] { "int", "List<int>" }, new[] { "int", "t:List<bool>", "null" })]
+    public async Task Switch_With_Mismatching_Generic_Type_Fails(SwitchType switchType, string[] typesToCheck, string[] casesChecked)
     {
-        var code = "using System.Collections.Generic;"
-            + Shared.Structs
-            + @"
-class C
-{
-    void M(Either<int, List<int>> x)
-    {
-        {|#0:switch|} (x.Value)
-        {
-            case int:
-                break;
-            {|#1:case List<bool>:|}
-                break;
-            case null:
-                break;
-        }
-    }
-}
-";
+        var code = Shared.GenerateSwitch(switchType, typesToCheck, casesChecked, new() { TagSwitch = true, Usings = ["System.Collections.Generic"] });
+
         var expectedNotExhaustive = VerifyCS.Diagnostic(EitherAnalyzer.NotExhaustiveId)
             .WithLocation(0)
             .WithArguments("Case", "List<int>", "is");
@@ -63,28 +31,13 @@ class C
         await VerifyCS.VerifyAnalyzerAsync(code, expectedNotExhaustive, expectedRedundantCase);
     }
 
-    [Fact]
-    public async Task Given_Switch_Statement_With_Partially_Mismatching_Generic_Type_Fail()
+    [Theory]
+    [InlineData(SwitchType.Stmt, new[] { "int", "Dictionary<int, string>" }, new[] { "int", "t:Dictionary<int, decimal>", "null" })]
+    [InlineData(SwitchType.Expr, new[] { "int", "Dictionary<int, string>" }, new[] { "int", "t:Dictionary<int, decimal>", "null" })]
+    public async Task Switch_With_Partially_Mismatching_Generic_Type_Fails(SwitchType switchType, string[] typesToCheck, string[] casesChecked)
     {
-        var code = "using System.Collections.Generic;"
-            + Shared.Structs
-            + @"
-class C
-{
-    void M(Either<int, Dictionary<int, string>> x)
-    {
-        {|#0:switch|} (x.Value)
-        {
-            case int:
-                break;
-            {|#1:case Dictionary<int, decimal>:|}
-                break;
-            case null:
-                break;
-        }
-    }
-}
-";
+        var code = Shared.GenerateSwitch(switchType, typesToCheck, casesChecked, new() { TagSwitch = true, Usings = ["System.Collections.Generic"] });
+
         var expectedNotExhaustive = VerifyCS.Diagnostic(EitherAnalyzer.NotExhaustiveId)
             .WithLocation(0)
             .WithArguments("Case", "Dictionary<int, string>", "is");
@@ -96,28 +49,13 @@ class C
         await VerifyCS.VerifyAnalyzerAsync(code, expectedNotExhaustive, expectedRedundantCase);
     }
 
-    [Fact]
-    public async Task Given_Switch_Statement_With_Partially_Mismatching_Nested_Generic_Type_Fail()
+    [Theory]
+    [InlineData(SwitchType.Stmt, new[] { "int", "HashSet<List<int>>" }, new[] { "int", "t:HashSet<List<decimal>>", "null" })]
+    [InlineData(SwitchType.Expr, new[] { "int", "HashSet<List<int>>" }, new[] { "int", "t:HashSet<List<decimal>>", "null" })]
+    public async Task Switch_With_Partially_Mismatching_Nested_Generic_Type_Fails(SwitchType switchType, string[] typesToCheck, string[] casesChecked)
     {
-        var code = "using System.Collections.Generic;"
-            + Shared.Structs
-            + @"
-class C
-{
-    void M(Either<int, HashSet<List<int>>> x)
-    {
-        {|#0:switch|} (x.Value)
-        {
-            case int:
-                break;
-            {|#1:case HashSet<List<decimal>>:|}
-                break;
-            case null:
-                break;
-        }
-    }
-}
-";
+        var code = Shared.GenerateSwitch(switchType, typesToCheck, casesChecked, new() { TagSwitch = true, Usings = ["System.Collections.Generic"] });
+
         var expectedNotExhaustive = VerifyCS.Diagnostic(EitherAnalyzer.NotExhaustiveId)
             .WithLocation(0)
             .WithArguments("Case", "HashSet<List<int>>", "is");
@@ -127,90 +65,72 @@ class C
             .WithArguments("HashSet<List<decimal>>");
 
         await VerifyCS.VerifyAnalyzerAsync(code, expectedNotExhaustive, expectedRedundantCase);
-
     }
 
-    [Fact]
-    public async Task Given_Switch_Statement_With_Matching_Delegate_Types_Succeed()
+    [Theory]
+    [InlineData(SwitchType.Stmt, new[] { "Action", "Func<string>" }, new[] { "Action", "Func<string>", "null" })]
+    [InlineData(SwitchType.Expr, new[] { "Action", "Func<string>" }, new[] { "Action", "Func<string>", "null" })]
+    public async Task Switch_With_Matching_Delegate_Types_Succeeds(SwitchType switchType, string[] typesToCheck, string[] casesChecked)
     {
-        var code = "using System;"
-            + Shared.Structs
-            + @"
-class C
-{
-    void M(Either<Action, Func<string>> x)
-    {
-        switch (x.Value)
-        {
-            case Action:
-                break;
-            case Func<string>:
-                break;
-            case null:
-                break;
-        }
-    }
-}
-";
+        var code = Shared.GenerateSwitch(switchType, typesToCheck, casesChecked, new() { TagSwitch = true, Usings = ["System"] });
         await VerifyCS.VerifyAnalyzerAsync(code);
-
     }
 
-//    [Fact]
-//    public async Task Given_Switch_Statement_With_Matching_Tuple_Types_Succeed()//problematic
-//    {
-//        var code = "using System.Collections.Generic;"
-//            + Shared.Structs
-//            + @"
-//class C
-//{
-//    void M(Either<(int, string), (string, decimal?)> x)
-//    {
-//        switch (x.Value)
-//        {
-//            case (int, string):
-//                break;
-//            case (string s, decimal? d):
-//                break;
-//            //case Person { Age: var a }:
-//            //    break;
-//        }
-//    }
-//}
+    //    [Fact]
+    //    public async Task Given_Switch_Statement_With_Matching_Tuple_Types_Succeed()//problematic
+    //    {
+    //        var code = "using System.Collections.Generic;"
+    //            + Shared.Structs
+    //            + @"
+    //class C
+    //{
+    //    void M(Either<(int, string), (string, decimal?)> x)
+    //    {
+    //        switch (x.Value)
+    //        {
+    //            case (int, string):
+    //                break;
+    //            case (string s, decimal? d):
+    //                break;
+    //            //case Person { Age: var a }:
+    //            //    break;
+    //        }
+    //    }
+    //}
 
-////public class Person
-////{
-////    public int Age { get; set; }
-////}
+    ////public class Person
+    ////{
+    ////    public int Age { get; set; }
+    ////}
 
-//";
-//        await VerifyCS.VerifyAnalyzerAsync(code);
-//    }
+    //";
+    //        await VerifyCS.VerifyAnalyzerAsync(code);
+    //    }
 
 
-//    [TestMethod]
-//    public async Task list_of_tuples() //problematic
-//    {
-//        var code = "using System.Collections.Generic;"
-//            + Shared.Structs
-//            + @"
-//class C
-//{
-//    void M(Either<List<(int, string)>, int> x)
-//    {
-//        switch (x.Value)
-//        {
-//            case int:
-//                break;
-//            case List<(int a, string b)>: //this fails!!!!
-//                break;
-//            case null:
-//                break;
-//        }
-//    }
-//}
+    //    [TestMethod]
+    //    public async Task list_of_tuples() //problematic
+    //    {
+    //        var code = "using System.Collections.Generic;"
+    //            + Shared.Structs
+    //            + @"
+    //class C
+    //{
+    //    void M(Either<List<(int, string)>, int> x)
+    //    {
+    //        switch (x.Value)
+    //        {
+    //            case int:
+    //                break;
+    //            case List<(int a, string b)>: //this fails!!!!
+    //                break;
+    //            case null:
+    //                break;
+    //        }
+    //    }
+    //}
 
-//";
-//        await VerifyCS.VerifyAnalyzerAsync(code);
-//    }
+    //";
+    //        await VerifyCS.VerifyAnalyzerAsync(code);
+    //    }
 }
