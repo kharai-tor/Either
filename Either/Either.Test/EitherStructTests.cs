a5079066b2cf43c5b86b8981ec05c3c3f99703e2
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2025-09-11 22:15:40 +0400

    Either<T1,..>.Thing returning default(T1) when non-explicitly initialized #1

---
+++ Either/Either.Test/EitherStructTests.cs
@@ -0,0 +1,22 @@
+﻿namespace RhymesOfUncertainty.Test;
+
+public class EitherStructTests
+{
+    private Either<int, bool> _z;
+
+    private Either<int, bool> Z { get; }
+
+    [Fact]
+    public void Either_Struct_With_Non_Nullable_Type_Args_Will_Never_Hold_A_Null_Value()
+    {
+        var x = new Either<int, bool>();
+        Assert.NotNull(x.Thing);
+
+        var y = default(Either<int, bool>);
+        Assert.NotNull(y.Thing);
+
+        Assert.NotNull(_z.Thing);
+
+        Assert.NotNull(Z.Thing);
+    }
+}
--- Either/Either/Either.cs
+++ Either/Either/Either.cs
@@ -2,90 +2,142 @@
 
 public struct Either<T1, T2>
 {
-    public object Thing { get; private set; }
-
-    public static implicit operator Either<T1, T2>(T1 t1)
-    {
-        return new Either<T1, T2> { Thing = t1 };
+    private bool _initialized;
+    private object _thing;
+    public object Thing 
+    { 
+        get
+        {
+            return _initialized ? _thing : default(T1);
+        }
+        private set
+        {
+            _initialized = true;
+            _thing = value;
+        }
     }
 
-    public static implicit operator Either<T1, T2>(T2 t2)
+    public static implicit operator Either<T1, T2>(T1 t)
     {
-        return new Either<T1, T2> { Thing = t2 };
+        return new Either<T1, T2> { Thing = t };
+    }
+
+    public static implicit operator Either<T1, T2>(T2 t)
+    {
+        return new Either<T1, T2> { Thing = t };
     }
 }
 
 public struct Either<T1, T2, T3>
 {
-    public object Thing { get; private set; }
-
-    public static implicit operator Either<T1, T2, T3>(T1 t1)
-    {
-        return new Either<T1, T2, T3> { Thing = t1 };
+    private bool _initialized;
+    private object _thing;
+    public object Thing 
+    { 
+        get
+        {
+            return _initialized ? _thing : default(T1);
+        }
+        private set
+        {
+            _initialized = true;
+            _thing = value;
+        }
     }
 
-    public static implicit operator Either<T1, T2, T3>(T2 t2)
+    public static implicit operator Either<T1, T2, T3>(T1 t)
     {
-        return new Either<T1, T2, T3> { Thing = t2 };
+        return new Either<T1, T2, T3> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3>(T3 t3)
+    public static implicit operator Either<T1, T2, T3>(T2 t)
     {
-        return new Either<T1, T2, T3> { Thing = t3 };
+        return new Either<T1, T2, T3> { Thing = t };
+    }
+
+    public static implicit operator Either<T1, T2, T3>(T3 t)
+    {
+        return new Either<T1, T2, T3> { Thing = t };
     }
 }
 
 public struct Either<T1, T2, T3, T4>
 {
-    public object Thing { get; private set; }
-
-    public static implicit operator Either<T1, T2, T3, T4>(T1 t1)
-    {
-        return new Either<T1, T2, T3, T4> { Thing = t1 };
+    private bool _initialized;
+    private object _thing;
+    public object Thing 
+    { 
+        get
+        {
+            return _initialized ? _thing : default(T1);
+        }
+        private set
+        {
+            _initialized = true;
+            _thing = value;
+        }
     }
 
-    public static implicit operator Either<T1, T2, T3, T4>(T2 t2)
+    public static implicit operator Either<T1, T2, T3, T4>(T1 t)
     {
-        return new Either<T1, T2, T3, T4> { Thing = t2 };
+        return new Either<T1, T2, T3, T4> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3, T4>(T3 t3)
+    public static implicit operator Either<T1, T2, T3, T4>(T2 t)
     {
-        return new Either<T1, T2, T3, T4> { Thing = t3 };
+        return new Either<T1, T2, T3, T4> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3, T4>(T4 t4)
+    public static implicit operator Either<T1, T2, T3, T4>(T3 t)
     {
-        return new Either<T1, T2, T3, T4> { Thing = t4 };
+        return new Either<T1, T2, T3, T4> { Thing = t };
+    }
+
+    public static implicit operator Either<T1, T2, T3, T4>(T4 t)
+    {
+        return new Either<T1, T2, T3, T4> { Thing = t };
     }
 }
 
 public struct Either<T1, T2, T3, T4, T5>
 {
-    public object Thing { get; private set; }
-
-    public static implicit operator Either<T1, T2, T3, T4, T5>(T1 t1)
-    {
-        return new Either<T1, T2, T3, T4, T5> { Thing = t1 };
+    private bool _initialized;
+    private object _thing;
+    public object Thing 
+    { 
+        get
+        {
+            return _initialized ? _thing : default(T1);
+        }
+        private set
+        {
+            _initialized = true;
+            _thing = value;
+        }
     }
 
-    public static implicit operator Either<T1, T2, T3, T4, T5>(T2 t2)
+    public static implicit operator Either<T1, T2, T3, T4, T5>(T1 t)
     {
-        return new Either<T1, T2, T3, T4, T5> { Thing = t2 };
+        return new Either<T1, T2, T3, T4, T5> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3, T4, T5>(T3 t3)
+    public static implicit operator Either<T1, T2, T3, T4, T5>(T2 t)
     {
-        return new Either<T1, T2, T3, T4, T5> { Thing = t3 };
+        return new Either<T1, T2, T3, T4, T5> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3, T4, T5>(T4 t4)
+    public static implicit operator Either<T1, T2, T3, T4, T5>(T3 t)
     {
-        return new Either<T1, T2, T3, T4, T5> { Thing = t4 };
+        return new Either<T1, T2, T3, T4, T5> { Thing = t };
     }
 
-    public static implicit operator Either<T1, T2, T3, T4, T5>(T5 t5)
+    public static implicit operator Either<T1, T2, T3, T4, T5>(T4 t)
     {
-        return new Either<T1, T2, T3, T4, T5> { Thing = t5 };
+        return new Either<T1, T2, T3, T4, T5> { Thing = t };
+    }
+
+    public static implicit operator Either<T1, T2, T3, T4, T5>(T5 t)
+    {
+        return new Either<T1, T2, T3, T4, T5> { Thing = t };
     }
 }
