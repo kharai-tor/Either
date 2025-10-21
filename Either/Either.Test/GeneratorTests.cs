487e780bce7b80cd852e8ba18f7345aacd6f9ea3
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2025-10-21 23:49:58 +0400

    Named union types - first step #7

---
+++ Either/Either.Test/GeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace RhymesOfUncertainty.Test;
+
+public class GeneratorTests
+{
+    [Fact]
+    public async Task Generator_With_2_Concrete_Type_Arguments_Works()
+    {
+        var context = new CSharpSourceGeneratorTest<NamedUnionGenerator, DefaultVerifier>();
+
+        context.TestCode = """
+            using System;
+            using RhymesOfUncertainty;
+
+            namespace RhymesOfUncertainty
+            {
+                [AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+                public class NamedUnionTypeAttribute : Attribute;
+            
+                public interface IEither<T1, T2>;
+            }
+
+            namespace Consumer
+            {
+                [NamedUnionType]
+                public partial struct IntOrBool : IEither<int, bool>;
+            }
+            """;
+
+        context.TestState.GeneratedSources.Add((typeof(NamedUnionGenerator), "IntOrBool.g.cs", NamedUnionGenerator.GenerateCode("Consumer", "IntOrBool", ["int", "bool"])));
+
+        await context.RunAsync();
+    }
+}
---
+++ Either/Either/NamedUnionTypeAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace RhymesOfUncertainty;
+
+[AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+public class NamedUnionTypeAttribute : Attribute;
