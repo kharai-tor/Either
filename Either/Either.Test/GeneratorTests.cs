c5ad329356ce0c689ab13d853e276ebd28d85db8
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2025-11-19 22:22:44 +0400

    Union generator for generic structs

--- Either/Either.Test/GeneratorTests.cs
+++ Either/Either.Test/GeneratorTests.cs
@@ -5,23 +5,25 @@ namespace RhymesOfUncertainty.Test;
 
 public class GeneratorTests
 {
+    private static readonly string Prefix = """
+        using System;
+        using RhymesOfUncertainty;
+        
+        namespace RhymesOfUncertainty
+        {
+            [AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+            public class NamedUnionTypeAttribute : Attribute;
+        
+            public interface IEither<T1, T2>;
+        }
+        """;
+
     [Fact]
     public async Task Generator_With_2_Concrete_Type_Arguments_Works()
     {
         var context = new CSharpSourceGeneratorTest<NamedUnionGenerator, DefaultVerifier>();
 
-        context.TestCode = """
-            using System;
-            using RhymesOfUncertainty;
-
-            namespace RhymesOfUncertainty
-            {
-                [AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
-                public class NamedUnionTypeAttribute : Attribute;
-            
-                public interface IEither<T1, T2>;
-            }
-
+        context.TestCode = Prefix + """
             namespace Consumer
             {
                 [NamedUnionType]
@@ -30,6 +32,24 @@ public class GeneratorTests
             """;
 
         context.TestState.GeneratedSources.Add((typeof(NamedUnionGenerator), "IntOrBool.g.cs", NamedUnionGenerator.GenerateCode("Consumer", "IntOrBool", ["int", "bool"])));
+
+        await context.RunAsync();
+    }
+
+    [Fact]
+    public async Task Generator_With_Generic_Type_Arguments_Works()
+    {
+        var context = new CSharpSourceGeneratorTest<NamedUnionGenerator, DefaultVerifier>();
+
+        context.TestCode = Prefix + """
+            namespace Consumer
+            {
+                [NamedUnionType]
+                public partial struct Result<T> : IEither<T, Exception>;
+            }
+            """;
+
+        context.TestState.GeneratedSources.Add((typeof(NamedUnionGenerator), "Result.g.cs", NamedUnionGenerator.GenerateCode("Consumer", "Result<T>", ["T", "System.Exception"])));
 
         await context.RunAsync();
     }
