51f1afba7326ae18b87d86acaaaf0dfdb2ef4271
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2025-12-01 23:00:47 +0400

    Named union types - interfaces #7

---
+++ Either/Either/IEither.cs
@@ -0,0 +1,46 @@
+namespace RhymesOfUncertainty;
+
+public interface IEither<T1, T2>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5, T6>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5, T6, T7>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5, T6, T7, T8>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+{
+    object Thing { get; }
+}
+
+public interface IEither<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+{
+    object Thing { get; }
+}
